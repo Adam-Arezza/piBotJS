24a98eeb63004c95cbcd780a9e2ad6c3d3200e0d
Adam Arezza <arezza.adam@gmail.com>
2019-10-12 17:48:24 -0400

    added servos to the J5 board

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ const board = new five.Board({
 board.on('ready', function() {
     console.log('Board is ready')
     const gripper = new five.Servo({
-        pin:0,
         controller:"PCA9658",
+        pin:2,
         range:[50,150]
     })
+    const joint1 = new five.Servo({
+        controller: "PCA9658",
+        pin:0,
+        range:[30,150]
+    })
+    const joint2 = new five.Servo({
+        controller:"PCA9658",
+        pin:1,
+        range:[30,150]
+    })
+
 
     gripper.center()
+    joint1.center()
+    joint2.center()
 
-    board.repl.inject({
-        gripper
+
+    this.repl.inject({
+        gripper, joint1, joint2
     })
 
 })
