c562a0762b876259e2b639eb5f86827f3d10bcb0
Adam Arezza <arezza.adam@gmail.com>
2019-10-13 14:50:26 -0400

    added motors

--- index.js
+++ index.js
@@ -5,36 +5,68 @@ const board = new five.Board({
     io: new Raspi()
 })
 
-board.on('ready', function() {
+function msg() {
+    console.log("function from outside the board")
+}
+
+board.on('ready', function () {
+    function turn(motors, direction, speed){
+        if(direction == "left") {
+            motors[0].forward(speed)
+            motors[1].reverse(speed)
+        }
+        if(direction == "right") {
+            motors[0].reverse(speed)
+            motors[1].forward(speed)
+        }
+    }
+
     console.log('Board is ready')
     const gripper = new five.Servo({
-        controller:"PCA9685",
-        pin:2,
-        range:[50,150]
+        controller: "PCA9685",
+        pin: 2,
+        range: [10, 150]
     })
     const joint1 = new five.Servo({
         controller: "PCA9685",
-        pin:0,
-        range:[30,150]
+        pin: 0,
+        range: [30, 165]
     })
     const joint2 = new five.Servo({
-        controller:"PCA9685",
-        pin:1,
-        range:[30,150]
+        controller: "PCA9685",
+        pin: 1,
+        range: [30, 180]
+    })
+    const motors = new five.Motors([
+        { pins: { dir: "P1-31", pwm: "P1-33" }, invertPWM },
+        { pins: { dir: "P1-35", pwm: "P1-37" }, invertPWM }
+    ])
+    const enable = new five.Pin({
+        pin: "P1-7",
+        type: "digital"
+    })
+    // gripper.center()
+    // joint1.center()
+    // joint2.center()
+    motors.on('forward', function(){
+        enable.high()
+        console.log("Drive motors enabled")
+    })
+    motors.on('reverse', function() {
+        enable.high()
+        console.log("Drive motors enabled")
+    })
+    motors.on('stop', function() {
+        enable.low()
+        console.log("Drive motors disabled")
     })
 
-
-    gripper.center()
-    joint1.center()
-    joint2.center()
-
-
     this.repl.inject({
-        gripper, joint1, joint2
+        gripper, joint1, joint2, motors, turn
     })
 
 })
 
-board.on('fail', function(event) {
+board.on('fail', function (event) {
     console.log(event)
 })
