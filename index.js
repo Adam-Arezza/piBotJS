6cc6491bb09016fa181e0635e163e317ac44a5d8
Adam Arezza <arezza.adam@gmail.com>
2019-10-14 11:59:23 -0400

    put pimotors and piarm into the board.ready function

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const five = require('johnny-five')
 const Raspi = require('raspi-io').RaspiIO
-const piMotors = require('./motors')
-const piArm = require('./arm')
 
 const board = new five.Board({
     io: new Raspi()
 })
 
 board.on('ready', function () {
-
     console.log('Board is ready')
+    const piMotors = require('./motors')
+    const piArm = require('./arm')
 
     this.repl.inject({
         piArm, piMotors
