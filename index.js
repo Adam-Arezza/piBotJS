a4b9f415f48cb78d0640351bf196845fc994d854
Adam Arezza <arezza.adam@gmail.com>
2019-12-14 14:43:39 -0500

    Added socketio and express for realtime control via web app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const five = require('johnny-five')
 // const Raspi = require('raspi-io').RaspiIO
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 const rpi = require('pi-io')
 
 const board = new five.Board({
@@ -20,13 +25,33 @@ board.on('ready', function () {
 
     proximity.on('change', (data) => distance = data)
 
-    setInterval( function() { console.log(distance)}, 1500)
-
-    this.repl.inject({
-        piArm, piMotors
+    io.on('connection', (socket) => {
+        console.log("connection successful")
+        socket.on("move", (dir) => {
+            switch(dir) {
+                case "forward":
+                    piMotors.motors.forward(255)
+                    break
+                case "reverse":
+                    piMotors.motors.reverse(255)
+                    break
+                default:
+                    console.log("not moving")
+            }
+        })
+        setInterval( function() {
+            io.emit('distance', distance)
+        }, 1000)
     })
+    // setInterval( function() { console.log(distance)}, 1500)
+
+    // this.repl.inject({
+    //     piArm, piMotors
+    // })
 })
 
 board.on('fail', function (event) {
     console.log(event)
 })
+
+http.listen(3000, ()=> console.log('listening on port 3000'))
