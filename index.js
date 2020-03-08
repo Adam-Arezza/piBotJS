facdee5ba82882568cab4e1b9f6eb0a22ae3fd3f
Adam Arezza <arezza.adam@gmail.com>
2020-03-07 20:51:57 -0500

    Added motors

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express()
 app.use(cors())
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-// const rpi = require('pi-io')
+const rpi = require('pi-io')
 const cv = require('opencv4nodejs')
-// const board = new five.Board({
-//     io: new rpi(),
-//     repl:false
-// })
+const board = new five.Board({
+    io: new rpi(),
+    repl:false
+})
 
 const cam = new cv.VideoCapture(0)
 const fps = 5
@@ -21,38 +21,38 @@ setInterval(() => {
     io.emit('videoData', img)
 }, 1000/fps)
 
-// board.on('ready', function () {
-//     console.log('Board is ready')
+board.on('ready', function () {
+    console.log('Board is ready')
 
-//     // const piMotors = require('./motors')
-//     // const piArm = require('./arm')
-//     // const prox = require('./distance')
+    const piMotors = require('./motors')
+    // const piArm = require('./arm')
+    // const prox = require('./distance')
 
-//     io.on('connection', (socket) => {
-//         console.log("connection successful")
-//         // socket.on("move", (dir) => {
-//         //     switch(dir) {
-//         //         case "forward":
-//         //             piMotors.enable.high()
-//         //             piMotors.motors.forward(255)
-//         //             break
-//         //         case "reverse":
-//         //             piMotors.enable.high()
-//         //             piMotors.motors.reverse(255)
-//         //             break
-//         //         case "stop":
-//         //             piMotors.enable.low()
-//         //             piMotors.motors.stop()
-//         //         default:
-//         //             console.log("not moving")
-//         //     }
-//         // })
-//     })
+    io.on('connection', (socket) => {
+        console.log("connection successful")
+        socket.on("move", (dir) => {
+            switch(dir) {
+                case "forward":
+                    piMotors.enable.high()
+                    piMotors.motors.forward(255)
+                    break
+                case "reverse":
+                    piMotors.enable.high()
+                    piMotors.motors.reverse(255)
+                    break
+                case "stop":
+                    piMotors.enable.low()
+                    piMotors.motors.stop()
+                default:
+                    console.log("not moving")
+            }
+        })
+    })
 
-// })
+})
 
-// board.on('fail', function (event) {
-//     console.log(event)
-// })
+board.on('fail', function (event) {
+    console.log(event)
+})
 
 http.listen(3000, ()=> console.log('listening on port 3000'))
